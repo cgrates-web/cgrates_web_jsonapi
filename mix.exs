a12cf9f1bd2ad490c75642ff5762ab13ed9bc739
max <maxim21214@gmail.com>
2017-09-13 15:48:35 +0700

    prepare db for tests

--- mix.exs
+++ mix.exs
@@ -67,6 +67,6 @@ defmodule CgratesWebJsonapi.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.load", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.create --quiet", "ecto.load", "ecto.migrate", "test"]]
   end
 end
