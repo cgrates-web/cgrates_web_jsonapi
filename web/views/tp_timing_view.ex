d87fb53a1352bdd3d9e9701c1ce6dd3e13ddb94d
Max <maxim21214@gmail.com>
2017-10-03 14:30:17 +0700

    Update tp_timing_view.ex

--- web/views/tp_timing_view.ex
+++ web/views/tp_timing_view.ex
@@ -2,5 +2,5 @@ defmodule CgratesWebJsonapi.TpTimingView do
   use CgratesWebJsonapi.Web, :view
   use JaSerializer.PhoenixView
 
-  attributes [:tpid, :tag, :years, :months, :month_days, :week_days, :time, :cerated_at]
+  attributes [:tpid, :tag, :years, :months, :month_days, :week_days, :time, :created_at]
 end
