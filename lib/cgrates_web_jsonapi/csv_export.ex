3afdd7429a417706eede793af0ab8a5185da5ed8
DanielYouCan <danbizarre@gmail.com>
2018-10-16 10:46:34 +0700

    Make build_raw_copy_query function private

--- lib/cgrates_web_jsonapi/csv_export.ex
+++ lib/cgrates_web_jsonapi/csv_export.ex
@@ -1,19 +1,11 @@
 defmodule CgratesWebJsonapi.CsvExport do
   @doc """
-  Builds sql query that will copy its result to csv header 
+  Builds sql query that will copy its result to csv header
   """
   def build_copy_query(query, values) do
     1..length(values)
       |> query_with_values(query, values)
       |> build_raw_copy_query
-  end
-
-  def build_raw_copy_query(query) do
-    """
-      COPY (
-        #{query}
-      ) TO STDOUT CSV HEADER
-    """
   end
 
   @doc """
@@ -24,5 +16,13 @@ defmodule CgratesWebJsonapi.CsvExport do
       {:ok, value} = Enum.fetch(values, n - 1)
       String.replace(acc, "$#{n}", "'#{value}'")
     end
+  end
+
+  defp build_raw_copy_query(query) do
+    """
+      COPY (
+        #{query}
+      ) TO STDOUT CSV HEADER
+    """
   end
 end
