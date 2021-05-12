b189bde5571ab454d3460c46f0f2ba3e376e2e9e
Roman Ustiantcev <roman@anyplay.audio>
2021-05-12 11:44:20 +0200

    swagger docs

--- lib/cgrates_web_jsonapi_web/controllers/cdr_extra_field_controller.ex
+++ lib/cgrates_web_jsonapi_web/controllers/cdr_extra_field_controller.ex
@@ -1,7 +1,18 @@
 defmodule CgratesWebJsonapiWeb.CdrExtraFieldController do
   use CgratesWebJsonapiWeb, :controller
+  use PhoenixSwagger
 
   alias CgratesWebJsonapi.Cdrs
+
+  swagger_path :index do
+    get("/api/cdr-extra-fields")
+    CommonSwaggerParams.authorization()
+    CommonSwaggerParams.content_type()
+
+    description("An array of string values of used extra_fields")
+
+    response(200, "OK")
+  end
 
   def index(conn, _) do
     render(conn, "index.json-api", data: Cdrs.extra())
